efbd001b8311a297002bfddb26f506d46b319de3
MixedMatched <30960626+MixedMatched@users.noreply.github.com>
2023-04-28 00:14:53 -0400

    update about

--- src/components/about.rs
+++ src/components/about.rs
@@ -5,6 +5,7 @@ pub fn About(cx: Scope) -> Element {
         div {
             h1 { "About" }
             p { "This is a website for practicing logic proofs." }
+            p { "Find the source code on " a { href: "https://github.com/MixedMatched/rclogic_src", "GitHub" } "." }
         }
     })
 }
